d3919abdf2e8775bef455166b0aee2bed8671ed6
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2019-08-26 21:26:57 -0300

    text robot]

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const readline = require('readline-sync')
-function start() {
+	const readline = require('readline-sync')
+const robots = {
+	text: require('./robots/texto.js')
+}
+async function start() {
     	const content = {}
-    
     	content.searchTheme = askAndReturnSearchTheme()
+	await robots.text(content)
 	function askAndReturnSearchTheme(){
 	return readline.question('Type a Wikipedia search Theme: ')
 	
 	}
-	console.log(content)
-
+	console.log(content.sourceContentOriginal)
 }
 start()
